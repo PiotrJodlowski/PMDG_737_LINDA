d79563ee0aaa23f461ccf6e29944b20ee0727892
Piotr Jodłowski <102728541+PiotrJodlowski@users.noreply.github.com>
2022-06-08 11:21:24 +0200

    PMDG737final_lua

---
+++ config-hid.lua
@@ -0,0 +1,8 @@
+-- PMDG 737NGX config-hid.lua @ 2110101324 (LINDA 3.2.6.1111) --
+
+-- ## Saitek Yoke ## (06A30BD30) ## {RD014800} ############
+
+
+-- ## Saitek Throttle ## (06A30C2D0) ## {NoSerNum} ############
+
+
